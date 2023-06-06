6483e5de6f7dfc42a7a5842d43cd276558a7081b
Giovana Lima <sdelgiov@amazon.com>
2023-06-05 23:24:05 -0300

    feat: adiciona getPart e ajusta nome dos comandos

--- src/EP/client/Client.java
+++ src/EP/client/Client.java
@@ -32,35 +32,38 @@ public class Client {
         }
         try {
             while (true) {
-                System.out.println("Enter command: ");
+                System.out.println("Insira um comando ou digite help para ver a lista de comandos disponíveis: ");
                 String command = scanner.nextLine();
 
                 switch (command) {
                     case "bind":
                         this.setRegistry();
                         break;
-                    case "showP":
-                        this.showP();
-                        break;
-                    case "clear":
-                        this.clear();
-                        break;
-                    case "addP":
-                        this.addP();
-                        break;
-                    case "listParts":
+                    case "listp":
                         this.listParts();
                         break;
-                    case "addSubPart":
+                    case "getp":
+                        this.getPart(); // implementar
+                        break;
+                    case "showp":
+                        this.showP();
+                        break;
+                    case "clearlist":
+                        this.clear();
+                        break;
+                    case "addsubpart":
                         this.addSubPart();
                         break;
-                    case "listSubParts":
+                    case "addp":
+                        this.addP();
+                        break;
+                    case "listsubparts":
                         this.listSubParts();
                         break;
                     case "help":
                         this.help();
                         break;
-                    case "exit":
+                    case "quit":
                         System.exit(0);
                         break;
                     default:
@@ -76,59 +79,6 @@ public class Client {
         }
     }
 
-    private void endCommand() {
-        int quantidade = 10; // Número de caracteres "-" a serem impressos
-
-        for (int i = 0; i < quantidade; i++) {
-            System.out.print("-");
-        }
-
-        System.out.println(); // Pular linha após a impressão dos caracteres "-"
-    }
-
-    private void help() {
-        System.out.println();
-        System.out.println("Comandos disponiveis:");
-        System.out.println("bind - conecta ao repositorio");
-        System.out.println("showP - seta a peca corrente e mostra seus dados");
-        System.out.println("listParts - lista as pecas do repositorio");
-        System.out.println("addP - adiciona uma peca ao repositorio atual");
-        System.out.println("clear - limpa a lista de subpecas corrente");
-        System.out.println("addSubPart - adiciona uma subpeca a lista de subpecas corrente");
-        System.out.println("listSubParts - lista as subpecas da peca corrente");
-        System.out.println("help - mostra os comandos disponiveis");
-        System.out.println("exit - sai do programa");
-        System.out.println("listCurrentSubParts - lista as subpecas da lista de subpeças corrente");
-    }
-
-    protected void listSubParts() {
-        try {
-            System.out.println("Subparts:");
-            System.out.println(this.subParts.getSubPartsCount());
-            if(this.subParts.getSubPartsCount() == 0) {
-                System.out.println("Nenhuma subpart adicionada ainda!");
-                return;
-            }
-            System.out.println(this.subParts.toString());
-            for(PartData subpart : this.subParts.getSubParts()) {
-                System.out.println(subpart.toString());
-            }
-        } catch (Exception e) {
-            System.out.println("Failed to list subparts!");
-            System.out.println(e.getMessage());
-        }
-    }
-
-    protected void setup(String[] args) throws NumberFormatException, RemoteException {
-        this.registry = LocateRegistry.getRegistry(args[0], Integer.parseInt(args[1]));
-        this.scanner = new Scanner(System.in);
-        this.registryMessages = new RegistryMessages(this.registry, this.scanner);
-        this.setRegistry();
-        this.subParts = new PartInventory();
-        this.currentPart = null;
-
-    }
-
     protected void setRegistry() {
         try {
             String serverName = this.registryMessages.getServerName();
@@ -136,6 +86,29 @@ public class Client {
         } catch (Exception e) {
             System.out.println("Failed to get registry!");
             throw new RuntimeException("Failed to get registry!");
+        }
+    }
+
+    protected void listParts() throws RemoteException {
+        System.out.println("Parts:");
+
+        List<Part> parts = this.partRepository.getParts();
+        if (parts.size() == 0) {
+            System.out.println("No parts found!");
+        }
+        for (Part part : parts) {
+            System.out.println(part.toString());
+        }
+    }
+
+    protected void getPart() {
+        System.out.println("Qual o id da peça? ");
+        Integer partId = Integer.parseInt(scanner.nextLine());
+        try {
+            this.currentPart = this.partRepository.getPartById(partId);
+            System.out.println("A peca foi setada como peca corrente");
+        } catch (Exception e) {
+            System.out.println("Failed to get part!");
         }
     }
 
@@ -156,20 +129,25 @@ public class Client {
         }
     }
 
-    protected void listParts() throws RemoteException {
-        System.out.println("Parts:");
-
-        List<Part> parts = this.partRepository.getParts();
-        if (parts.size() == 0) {
-            System.out.println("No parts found!");
-        }
-        for (Part part : parts) {
-            System.out.println(part.toString());
-        }
-    }
-
     protected void clear() {
         this.subParts = new PartInventory();
+    }
+
+    protected void addSubPart() {
+        try {
+            System.out.println("Qual o id da subpeça? ");
+            Integer subPartId = Integer.parseInt(scanner.nextLine());
+
+            System.out.println("Quantas peças deseja adicionar? ");
+            Integer subPartQuantity = Integer.parseInt(scanner.nextLine());
+
+            System.out.println("Em qual repositorio a peça se encontra? ");
+            String subPartRepositoryName = scanner.nextLine();
+
+            this.subParts.addSubPart(subPartId, subPartQuantity, subPartRepositoryName);
+        } catch (Exception e) {
+            System.out.println("Failed to add subpart!");
+        }
     }
 
     protected void addP() {
@@ -222,20 +200,55 @@ public class Client {
         }
     }
 
-    protected void addSubPart() {
+    protected void listSubParts() {
         try {
-            System.out.println("Qual o id da subpeça? ");
-            Integer subPartId = Integer.parseInt(scanner.nextLine());
-
-            System.out.println("Quantas peças deseja adicionar? ");
-            Integer subPartQuantity = Integer.parseInt(scanner.nextLine());
-
-            System.out.println("Em qual repositorio a peça se encontra? ");
-            String subPartRepositoryName = scanner.nextLine();
-
-            this.subParts.addSubPart(subPartId, subPartQuantity, subPartRepositoryName);
+            System.out.println("Subparts:");
+            System.out.println(this.subParts.getSubPartsCount());
+            if(this.subParts.getSubPartsCount() == 0) {
+                System.out.println("Nenhuma subpart adicionada ainda!");
+                return;
+            }
+            System.out.println(this.subParts.toString());
+            for(PartData subpart : this.subParts.getSubParts()) {
+                System.out.println(subpart.toString());
+            }
         } catch (Exception e) {
-            System.out.println("Failed to add subpart!");
+            System.out.println("Failed to list subparts!");
+            System.out.println(e.getMessage());
         }
+    }
+
+    private void help() {
+        System.out.println();
+        System.out.println("Comandos disponiveis:");
+        System.out.println("bind - conecta ao repositorio");
+        System.out.println("listp - lista as pecas do repositorio");
+        System.out.println("getp - seta a peca corrente");
+        System.out.println("showp - seta a peca corrente e mostra seus dados");
+        System.out.println("clearlist - limpa a lista de subpecas corrente");
+        System.out.println("addsubpart - adiciona uma subpeca a lista de subpecas corrente");
+        System.out.println("addp - adiciona uma peca ao repositorio atual");
+        System.out.println("listsubparts - lista as subpecas da peca corrente");
+        System.out.println("help - mostra os comandos disponiveis");
+        System.out.println("quit - sai do programa");
+    }
+
+    private void endCommand() {
+        int quantidade = 10; // Número de caracteres "-" a serem impressos
+
+        for (int i = 0; i < quantidade; i++) {
+            System.out.print("-");
+        }
+
+        System.out.println(); // Pular linha após a impressão dos caracteres "-"
+    }
+
+    protected void setup(String[] args) throws NumberFormatException, RemoteException {
+        this.registry = LocateRegistry.getRegistry(args[0], Integer.parseInt(args[1]));
+        this.scanner = new Scanner(System.in);
+        this.registryMessages = new RegistryMessages(this.registry, this.scanner);
+        this.setRegistry();
+        this.subParts = new PartInventory();
+        this.currentPart = null;
     }
 }
